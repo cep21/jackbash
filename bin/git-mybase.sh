d7b5cc8c4400e41500601efbed235aa1256e4c18
jlindamood <jlindamood@facebook.com>
2009-01-18 22:06:49 -0800

    Update git mybase.sh to work with any name of svn ref

--- bin/git-mybase.sh
+++ bin/git-mybase.sh
@@ -3,6 +3,6 @@
 SVN_REF=`git config svn-remote.svn.fetch | sed s/.*://`
 OLD_SVN=`git rev-parse $SVN_REF`
 git svn fetch > /dev/null
-git rebase git-svn > /dev/null
+git rebase $SVN_REF > /dev/null
 git log $OLD_SVN..$SVN_REF --stat=80,120 --pretty=format:'%Cred%an%Creset %Cgreen%H%Creset %s'
 
