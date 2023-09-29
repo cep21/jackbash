2e5de3a0e8f8fcfce7c0e010b9a4bdaa1c50fa1a
Jack Lindamood <jack@cresta.ai>
2023-09-29 13:38:51 -0700

    feat: helper for updating git dirs

---
+++ bin/update-all-git-dirs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update all .git directories that are a direct subdirectory of $(pwd)
+
+set -euo pipefail
+
+if [[ "${DEBUG-}" == "true" ]]; then
+  set -x
+fi
+
+function is_clean() {
+  [[ -z "$(git status -s)" ]]
+}
+
+function branch_name() {
+  branch_name=$(git symbolic-ref -q HEAD)
+  branch_name=${branch_name##refs/heads/}
+  branch_name=${branch_name:-HEAD}
+  echo "$branch_name"
+}
+
+function update_dir() {
+  cd $d
+  if [ ! -d .git ]; then
+    echo "$d is not a git directory: skipping"
+    exit 0
+  fi
+  git fetch -av
+  if ! is_clean ; then
+    echo "$d is not clean: skipping"
+    exit 0
+  fi
+  if [[ "$(branch_name)" == "main" ]]; then
+    git rebase origin/main
+  fi
+}
+
+for d in */ ; do
+(
+  echo "On directory $d"
+  update_dir "$d"
+)
+done
+
