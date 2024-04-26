bd7904c8ded1e6478b6e8d8210510609e278fc83
Jack Lindamood <jack@x.ai>
2024-04-25 21:54:02 -0700

    feat: netshoot on priviledged host

--- bin/kc-netshoot.sh
+++ bin/kc-netshoot.sh
@@ -3,5 +3,27 @@ set -exuo pipefail
 if [[ "${1-}" == "" ]]; then
 	kubectl run "netshoot-$(whoami)" --rm -i --tty --image nicolaka/netshoot -- /bin/bash
 else
-	kubectl run "netshoot-$(whoami)" --rm -i --tty --image nicolaka/netshoot --overrides="{ \"spec\": { \"tolerations\": [ { \"key\": \"\", \"operator\": \"Exists\" } ], \"nodeSelector\": { \"kubernetes.io/hostname\": \"${1}\" } } }" -- /bin/bash
+	kubectl run "netshoot-$(whoami)" --rm -i --tty --image nicolaka/netshoot \
+		--overrides='{ 
+		"spec": {
+			"hostNetwork": true,
+			"tolerations": [{
+				"key": "",
+				"operator": "Exists"
+			}],
+			"nodeSelector": {
+				"kubernetes.io/hostname": "'${1}'"
+			},
+			"containers": [{
+				"name": "netshoot",
+				"image": "nicolaka/netshoot",
+				"securityContext": {
+					"privileged": true
+				},
+				"command": ["/bin/bash"],
+				"stdin": true,
+				"tty": true
+			}]
+		}
+	}'
 fi
